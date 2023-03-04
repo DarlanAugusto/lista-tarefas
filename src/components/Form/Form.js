04ea00da351e68ee6d6503f5e3ae2fae0925a480
Darlan <darlansilv4@gmail.com>
2023-03-03 21:44:54 -0300

    Estilizando tarefas

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,34 +1,49 @@
 import React, { Component } from 'react';
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaTrashAlt } from 'react-icons/fa';
 
 import './Form.css';
 
 export default class Form extends Component {
   state = {
-    novaTarefa: '',
+    newTask: '',
+    tasks: ['Faculdade', 'Trabalhar', 'Academia', 'Estudar'],
   };
 
   handleInputChange = (event) => {
     this.setState({
-      novaTarefa: event.target.value,
+      newTask: event.target.value,
     });
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { newTask, tasks } = this.state;
 
     return (
-      <form action="#" className="form">
-        <input
-          type="text"
-          className="input"
-          onChange={this.handleInputChange}
-          value={novaTarefa}
-        />
-        <button className="button" type="submit">
-          <FaPlus />
-        </button>
-      </form>
+      <div>
+        <form action="#" className="form">
+          <input
+            type="text"
+            className="input"
+            onChange={this.handleInputChange}
+            value={newTask}
+          />
+          <button className="button" type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <li key={task}>
+              {task}
+              <div>
+                <FaEdit className="edit" />
+                <FaTrashAlt className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,7 +9,7 @@ export default class Main extends Component {
   render() {
     return (
       <div className="main">
-        <h1>Lista de Tarefas</h1>
+        <h1>List of Tasks</h1>
         <Form />
       </div>
     );
